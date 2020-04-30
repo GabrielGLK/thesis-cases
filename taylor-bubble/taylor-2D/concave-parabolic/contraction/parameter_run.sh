bbe0a084c104bd430cf296e526641fb4412c32fe
GabrielGLK <glk1206@hotmail.com>
2020-04-30 15:10:04 +0200

    add other singularities

---
+++ taylor-bubble/taylor-2D/concave-parabolic/contraction/parameter_run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+
+/******************** N_f &Eo &RHOR &MUR &l_taylor  ***********************/
+double N_f = 100;
+double Eo = 50;
+double RHOR = 1000;
+double MUR = 100;
+double l_taylor = 6; //bubble length ratio
