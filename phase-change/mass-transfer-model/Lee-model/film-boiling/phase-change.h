5d4b4996808439838cdf1ca247f746187765191c
GabrielGLK <glk1206@hotmail.com>
2020-05-07 18:26:46 +0200

    modify Lee model

--- phase-change/mass-transfer-model/Lee-model/film-boiling/phase-change.h
+++ phase-change/mass-transfer-model/Lee-model/film-boiling/phase-change.h
@@ -11,6 +11,43 @@ attribute {
   double rho;
 }
 
+
+// constant mass source scalar function
+void mass_source(scalar f, scalar m)
+{
+  foreach()
+  {
+    if(f[]>1e-12&&f[]<1 - 1e-12)
+      m[] = 0.05;
+    else
+      m[] = 0;
+  }
+  boundary({f,m});
+}
+
+// delta_s function
+void delta_magnini(scalar f, scalar delta ) {
+
+  foreach()  {
+    f[] = clamp(f[], 0., 1.);
+    double df, df2;
+    df2 = 0.;
+    df = 0;
+    foreach_dimension () {
+      df += (f[-1,1] + 2*f[0,1] + f[1,1] - f[-1,-1] - 2*f[0,-1] - f[1,-1])/(8*Delta);
+      df2 = sq(df);
+    }
+    delta[] = sqrt(df2);
+  }
+  boundary({delta});
+}
+
+void smoother(scalar fr, scalar ff) {
+  foreach()  
+    ff[] = (fr[-1,1] + 2*fr[-1,0] + fr[-1,-1] + fr[1,1] + 2*fr[1,0] + fr[1,-1] + 2*fr[0,1] + 4*fr[0,0] + 2*fr[0,-1])/16;
+  boundary({fr,ff});
+}
+
 /****************** Lee-model****************************/
 void lee_mass(scalar f, scalar tr, scalar m, double L_h){
 
@@ -22,9 +59,9 @@ void lee_mass(scalar f, scalar tr, scalar m, double L_h){
   {
     if(f[]>1e-12&&f[]<1-1e-12)
       {
-        r_i[] = tr.tr_eq/(L_h*(0.5+0.5*f[])*Delta*f[]*rho1);//mass transfer intensity factor expression
-        // An explicit expression of the empirical factor in a widely used phase change model
-        m[] = -r_i[]*rho1*f[]*(tr[] - tr.tr_eq)/tr.tr_eq;
+      r_i[] = 40*tr.tr_eq/(L_h*(0.5+0.5*f[])*Delta*f[]*rho1);//mass transfer intensity factor expression
+      // An explicit expression of the empirical factor in a widely used phase change model
+      m[] = -r_i[]*rho1*f[]*(tr[] - tr.tr_eq)/tr.tr_eq;
       }
     else
       m[] = 0;
@@ -52,7 +89,7 @@ mgstats heat_source (struct Heat_Source p)
   face vector diffusion_coef[];
 
   foreach()
-    dirichlet_source_term[] = cm[]*m[]*L_h/(tr.rho*tr.cp);
+    dirichlet_source_term[] = (tr.inverse?1:-1)*m[]*L_h/(tr.cp*tr.rho);
   foreach_face()
     diffusion_coef.x[] = fm.x[]*tr.D;
 
@@ -88,8 +125,124 @@ foreach()
 boundary({mass_source, b , c});
 
 foreach_face()
-  mass_diffusion_coef.x[] = fm.x[]*sq(5*Delta);// control the mass diffusion amplitude
+  mass_diffusion_coef.x[] = fm.x[]*sq(1.5*Delta);// control the mass diffusion amplitude
 boundary((scalar *){mass_diffusion_coef});
 
 return diffusion (tr, dt, D = mass_diffusion_coef, r = mass_source, beta = b, theta = c);
+}
+
+/****************** mass transfer model-4 (calculate phase-change velocity -> mass transfer rate)****************************/
+void mass_dot_4 (scalar f, scalar tr,  scalar m, scalar mm, double L_h) {
+  face vector v_pc[];
+
+  foreach()
+    f[] = clamp(f[], 0., 1.);
+  boundary ({f, tr});
+  
+  face vector gtr[];
+  foreach_face()
+    gtr.x[] = (tr[] - tr[-1])/Delta;
+  boundary((scalar*){gtr});
+
+  vector n[];
+  compute_normal (f, n);
+  
+  foreach_face() {
+    v_pc.x[] = 0.;
+
+    if (interfacial(point, f) || interfacial(neighborp(-1), f) ) {
+      coord nf;
+      foreach_dimension()
+        nf.x = 0.;
+      if (interfacial(point, f)) {
+        foreach_dimension()
+          nf.x += n.x[];
+      }
+      if (interfacial(neighborp(-1), f)) {
+        nf.x += n.x[-1];
+        nf.y += n.y[-1];
+      }
+   
+
+      double norm = 0.;
+      foreach_dimension()
+        norm += fabs(nf.x);
+      foreach_dimension()
+        nf.x /= (tr.inverse ? -norm : norm);
+      
+      
+      if (nf.x > 0.) {
+        v_pc.x[] = (fabs(nf.x)*gtr.x[1, 0] + fabs(nf.y)*(nf.y > 0. ? gtr.x[1, 1] : gtr.x[1, -1]));
+      }
+      else if (nf.x < 0.) {
+        v_pc.x[] = (fabs(nf.x)*gtr.x[-1, 0]+ fabs(nf.y)*(nf.y > 0. ? gtr.x[-1, 1] : gtr.x[-1, -1]));
+      }
+    }
+  }
+  boundary((scalar *){v_pc});
+  foreach()
+    {
+      m[] = 0;
+      mm[] = 0;
+    foreach_dimension()
+    {if(interfacial(point,f))
+      {
+        m[] += tr.lambda*v_pc.x[]/L_h*fabs((f[]-f[-1])/Delta);
+        mm[] += tr.lambda*v_pc.x[]/L_h*n.x[];
+      }
+     else
+      {
+        m[] += 0;
+        mm[] += 0;
+      }
+    }
+    }
+  boundary({m});
+}
+
+/****************** equilibrium saturated temperature on interface *************************/
+struct Dirichlet_Diffusion {
+  // mandatory
+  scalar tr;
+  scalar f;
+  int max_level;
+  double dt;
+  double time_factor;
+  // optional
+  scalar tr_op; // default uniform 1.
+};
+
+mgstats dirichlet_diffusion (struct Dirichlet_Diffusion p) {
+  
+  scalar tr = p.tr, f = p.f, tr_op = automatic (p.tr_op);
+  int max_level = p.max_level;
+  double time_factor = p.time_factor;
+
+  foreach()
+    f[] = clamp(f[], 0., 1.);
+  boundary ({f, tr});
+  if (p.tr_op.i)
+    boundary ({tr_op});
+
+  scalar volumic_metric[], dirichlet_source_term[], dirichlet_feedback_term[];
+  face vector diffusion_coef[];
+
+  foreach() {
+    volumic_metric[] = cm[];
+    if (p.tr_op.i)  
+      dirichlet_source_term[] = cm[]*tr_op[]*tr.tr_eq*tr.D*time_factor*(tr.inverse ?
+                                  f[] : 1. - f[])*sq((1<<max_level)/L0);
+    else
+      dirichlet_source_term[] = cm[]*tr.tr_eq*tr.D*time_factor*(tr.inverse ?
+                                  f[] : 1. - f[])*sq((1<<max_level)/L0);
+    dirichlet_feedback_term[] = - cm[]*tr.D*time_factor*(tr.inverse ?
+                                  f[] : 1. - f[])*sq((1<<max_level)/L0);
+  }
+  foreach_face()
+    diffusion_coef.x[] = fm.x[]*tr.D;
+
+  boundary({volumic_metric, dirichlet_source_term, dirichlet_feedback_term, diffusion_coef});
+  
+  return diffusion (tr, dt, D = diffusion_coef, r = dirichlet_source_term,
+			              beta = dirichlet_feedback_term, theta = volumic_metric);
 }
